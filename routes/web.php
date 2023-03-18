e60b72d5da3cdb15009f6683830a50d8bc615d41
B.L.S. Thathsara <65841165+BLSThathsara20@users.noreply.github.com>
2023-03-18 14:20:35 +0530

    FFirst Commit Signed-off-by: B.L.S. Thathsara <65841165+BLSThathsara20@users.noreply.github.com>

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\CategoriesController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::get('/template', function () {
+    return view('layouts.master');
+});
+
+Route::middleware(['auth:sanctum'])->group(function(){
+    //Category
+    Route::resource('categories', CategoriesController::class);
+});
